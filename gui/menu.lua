5ec1c9bb7c3e9cce1bd17d037971680061db097e
Mathieu Jasinski <mathieu.jasinski@epitech.eu>
2016-10-13 15:43:03 +0200

    highlight the selection on menu

--- gui/menu.lua
+++ gui/menu.lua
@@ -2,7 +2,7 @@
 
 
 function menu_draw( ... )
-    menus = {{"Solo", "Duo", "Option", "Manual", "Exit",},
+    menus = {{"Solo", "Duo", "Options", "Manual", "Exit",},
     {"line1","line2","line3","line4",},
     {"line1","line2","line3","line4",}}
 
@@ -10,18 +10,28 @@ function menu_draw( ... )
     for i=1, #menus[c_menu] do
         --love.graphics.print(c_item, 300, 360)
         if c_item == 1 then
+            love.graphics.setColor(255, 255, 255, 255)
+            love.graphics.rectangle( "fill", 390, 295 + 20 * c_item, 80, 20 )
             love.graphics.setColor(100, 145, 125, 100)
             love.graphics.print(menus[c_menu][c_item], 400, 300 + 20 * c_item)
         elseif c_item == 2 then
+            love.graphics.setColor(255, 255, 255, 255)
+            love.graphics.rectangle( "fill", 390, 295 + 20 * c_item, 80, 20 )
             love.graphics.setColor(100, 145, 125, 100)
             love.graphics.print(menus[c_menu][c_item], 400, 300 + 20 * c_item)
         elseif c_item == 3 then
+            love.graphics.setColor(255, 255, 255, 255)
+            love.graphics.rectangle( "fill", 390, 295 + 20 * c_item, 80, 20 )
             love.graphics.setColor(100, 145, 125, 100)
             love.graphics.print(menus[c_menu][c_item], 400, 300 + 20 * c_item)
         elseif c_item == 4 then
+            love.graphics.setColor(255, 255, 255, 255)
+            love.graphics.rectangle( "fill", 390, 295 + 20 * c_item, 80, 20 )
             love.graphics.setColor(100, 145, 125, 100)
             love.graphics.print(menus[c_menu][c_item], 400, 300 + 20 * c_item)
         elseif c_item == 0 then
+            love.graphics.setColor(255, 255, 255, 255)
+            love.graphics.rectangle( "fill", 390, 295 + 20 * 5, 80, 20 )
             love.graphics.setColor(100, 145, 125, 100)
             love.graphics.print(menus[c_menu][5], 400, 400)
             if love.keyboard.isDown('return') then
