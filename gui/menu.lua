1914452464bc53ff36b90f00b04b2c6da14b10da
Mathieu Jasinski <mathieu.jasinski@epitech.eu>
2016-10-13 15:06:47 +0200

    Visibility of menu

--- gui/menu.lua
+++ gui/menu.lua
@@ -2,31 +2,32 @@
 
 
 function menu_draw( ... )
-	menus = {{"Solo", "Duo", "Option", "Manual", "Exit",},
- 			{"line1","line2","line3","line4",},
- 			{"line1","line2","line3","line4",}}
-	
-	for i=1, #menus[c_menu] do
-		--love.graphics.print(c_item, 300, 360)
- 		if c_item == 1 then
-			love.graphics.setColor(100, 145, 125, 100)
-	   		love.graphics.print(menus[c_menu][c_item], 400, 300 + 20 * c_item)
-	   	elseif c_item == 2 then
-			love.graphics.setColor(100, 145, 125, 100)
-	   		love.graphics.print(menus[c_menu][c_item], 400, 300 + 20 * c_item)
-   		elseif c_item == 3 then
-			love.graphics.setColor(100, 145, 125, 100)
-	   		love.graphics.print(menus[c_menu][c_item], 400, 300 + 20 * c_item)
-   		elseif c_item == 4 then
-      		love.graphics.setColor(100, 145, 125, 100)
-	   		love.graphics.print(menus[c_menu][c_item], 400, 300 + 20 * c_item)
-   		elseif c_item == 0 then
-      		love.graphics.setColor(100, 145, 125, 100)
-	   		love.graphics.print(menus[c_menu][5], 400, 400)
-	   		if love.keyboard.isDown('return') then
-	   			love.event.quit()
-	   		end
-      	end
-		love.graphics.print(menus[c_menu][i], 400, 300 + 20 * i)
+    menus = {{"Solo", "Duo", "Option", "Manual", "Exit",},
+    {"line1","line2","line3","line4",},
+    {"line1","line2","line3","line4",}}
+
+    love.graphics.rectangle( "fill", 380, 300, 100, 140 )
+    for i=1, #menus[c_menu] do
+        --love.graphics.print(c_item, 300, 360)
+        if c_item == 1 then
+            love.graphics.setColor(100, 145, 125, 100)
+            love.graphics.print(menus[c_menu][c_item], 400, 300 + 20 * c_item)
+        elseif c_item == 2 then
+            love.graphics.setColor(100, 145, 125, 100)
+            love.graphics.print(menus[c_menu][c_item], 400, 300 + 20 * c_item)
+        elseif c_item == 3 then
+            love.graphics.setColor(100, 145, 125, 100)
+            love.graphics.print(menus[c_menu][c_item], 400, 300 + 20 * c_item)
+        elseif c_item == 4 then
+            love.graphics.setColor(100, 145, 125, 100)
+            love.graphics.print(menus[c_menu][c_item], 400, 300 + 20 * c_item)
+        elseif c_item == 0 then
+            love.graphics.setColor(100, 145, 125, 100)
+            love.graphics.print(menus[c_menu][5], 400, 400)
+            if love.keyboard.isDown('return') then
+                love.event.quit()
+            end
+        end
+        love.graphics.print(menus[c_menu][i], 400, 300 + 20 * i)
     end
 end
