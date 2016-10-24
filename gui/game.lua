80c2006ae0940490478df64b912fd8f1fdf9f131
F0xm0rtal <bavoysi@gmail.com>
2016-10-24 15:20:14 +0200

    corectif grille

--- gui/game.lua
+++ gui/game.lua
@@ -7,12 +7,12 @@ function game_draw()
     love.graphics.print(my, 10, 25)
     
     --draw plateau
-    for i=1, 770, 40 do
-	  love.graphics.line(50, 50+i, 810, 50+i)
-	  love.graphics.line(50+i, 50, 50+i, 810)
+    for i=1, 750, 40 do
+	  love.graphics.line(50, 50+i, 770, 50+i)
+	  love.graphics.line(50+i, 50, 50+i, 770)
     end
-	for e=1,18 do
-		for i=1,18 do
+	for e=1,17 do
+		for i=1,17 do
 	    	love.graphics.print(p_goban[e][i], 80 + 40 * (i-1), 80 + 40 * (e-1))	
     	end
 	end
@@ -21,9 +21,9 @@ end
 function init_aray()
 	array = {}
 
-	for i=1,18 do
+	for i=1,17 do
    		array[i] = {}
-   		for j=1,18 do
+   		for j=1,17 do
       		array[i][j] = 0
    		end
 	end
